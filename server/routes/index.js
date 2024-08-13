c65f010e00da7a3d95f9ace292fef89bf8ee6c58
thatSaksham <devyani2497.be21@chitkara.edu.in>
2024-08-14 02:17:06 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ router.use("/api", apiRoutes);
 // connect with react router
 // serve up react front-end in production
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../../build/index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 module.exports = router;
